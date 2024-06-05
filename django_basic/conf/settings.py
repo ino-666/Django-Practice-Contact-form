8477c91998fd6bac88591cb3810954bdfb2c7506
ino-666 <ennbiee_410@outlook.jp>
2024-06-05 16:48:19 +0900

    register adminsite application with django project

--- django_basic/conf/settings.py
+++ django_basic/conf/settings.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'contacts.apps.ContactsConfig',
+    'adminsite.apps.AdminsiteConfig',
     'crud.apps.CrudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
